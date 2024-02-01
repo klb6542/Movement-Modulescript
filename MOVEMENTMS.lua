ad0a356b7b1fa4c7418526935cea2fa27f81fdc5
klb6542 <147992430+klb6542@users.noreply.github.com>
2024-02-01 17:59:25 -0500

    Update MOVEMENTMS.lua

--- MOVEMENTMS.lua
+++ MOVEMENTMS.lua
@@ -1,40 +1,38 @@
---!strict
-
 type MovementStruct = {
 	Player: Player,
+	PlayerUserId: number,
 	CurrentSpeed: number,
 	CurrentJumpPower: number
 }
 
-local MOVEMENTMS = {}
+local MOVEMENTMS = {Cache = {} :: MovementStruct} -- Cast Cache type to MovementStruct type
 MOVEMENTMS.__index = MOVEMENTMS
 
-MOVEMENTMS.Cache = {} :: MovementStruct -- Cast type to MovementStruct
-
 function AddCache(self: MovementStruct)
-	MOVEMENTMS.Cache[self.Player] = self
+	MOVEMENTMS.Cache[self.Player.UserId] = self
 end
 
 function MOVEMENTMS.new(Player: Player)
 	local self: MovementStruct = {
 		Player = Player,
+		PlayerUserId = Player.UserId,
 		CurrentSpeed = 0,
 		CurrentJumpPower = 0
 	}
 
 	AddCache(self)
 	return setmetatable(self, MOVEMENTMS)
-end
+end 
 
-function MOVEMENTMS:GetCache(): MovementStruct | nil
-	return MOVEMENTMS.Cache[self.Player]
+function MOVEMENTMS:GetCache(Player: Player?): {[Player]: MovementStruct} | MovementStruct | {}
+	return if self.Cache[Player] then self.Cache[Player.UserId] else self.Cache
 end
 
 function MOVEMENTMS:Update()
 	local Character = self.Player.Character
 	local Humanoid = if Character then Character:FindFirstChildOfClass("Humanoid") else nil
 
-	if not Humanoid then warn(`Could not update speed for {self.Player.DisplayName}`) return end
+	if not Humanoid then warn(`Could not update movement for {self.Player.DisplayName}`) return end
 
 	Humanoid.WalkSpeed = self.CurrentSpeed
 	Humanoid.JumpPower = self.CurrentJumpPower
@@ -58,10 +56,8 @@ function MOVEMENTMS:GetJumpPower(): number
 	return self.CurrentJumpPower
 end
 
-function MOVEMENTMS:KillSelf(Player: Player?) -- Garbage collector
-	self.Player = self.Player or Player
-
-	MOVEMENTMS.Cache[self.Player] = nil
+function MOVEMENTMS:KillSelf() -- Garbage collector
+	MOVEMENTMS.Cache[self.Player.UserId] = nil
 	setmetatable(self, nil)
 	table.clear(self :: any)
 end
