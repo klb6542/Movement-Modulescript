0bf7e9bbf9abe5544c728a480930334dfa1101b0
klb6542 <147992430+klb6542@users.noreply.github.com>
2024-01-31 22:50:42 -0500

    Update MOVEMENTMS.lua

--- MOVEMENTMS.lua
+++ MOVEMENTMS.lua
@@ -1,38 +1,69 @@
+--!strict
+
+type MovementStruct = {
+	Player: Player,
+	CurrentSpeed: number,
+	CurrentJumpPower: number
+}
+
 local MOVEMENTMS = {}
 MOVEMENTMS.__index = MOVEMENTMS
 
+MOVEMENTMS.Cache = {} :: MovementStruct -- Cast type to MovementStruct
+
+function AddCache(self: MovementStruct)
+	MOVEMENTMS.Cache[self.Player] = self
+end
+
 function MOVEMENTMS.new(Player: Player)
-	return setmetatable({
+	local self: MovementStruct = {
 		Player = Player,
 		CurrentSpeed = 0,
 		CurrentJumpPower = 0
-	}, MOVEMENTMS)
+	}
+
+	AddCache(self)
+	return setmetatable(self, MOVEMENTMS)
+end
+
+function MOVEMENTMS:GetCache(): MovementStruct | nil
+	return MOVEMENTMS.Cache[self.Player]
 end
 
 function MOVEMENTMS:Update()
 	local Character = self.Player.Character
 	local Humanoid = Character or nil
-	
+
 	if not Humanoid then warn(`Could not update speed for {self.Player.DisplayName}`) return end
-	
+
 	Humanoid.WalkSpeed = self.CurrentSpeed
 	Humanoid.JumpPower = self.CurrentJumpPower
 end
 
 function MOVEMENTMS:SetSpeed(NewSpeed: number)
 	self.CurrentSpeed = NewSpeed
-end
- 
-function MOVEMENTMS:GetSpeed(): number
-	return self.CurrentSpeed
+	AddCache(self)
 end
 
 function MOVEMENTMS:SetJumpPower(NewJumpPower: number)
 	self.CurrentJumpPower = NewJumpPower
+	AddCache(self)
+end
+
+function MOVEMENTMS:GetSpeed(): number
+	return self.CurrentSpeed
 end
 
 function MOVEMENTMS:GetJumpPower(): number
 	return self.CurrentJumpPower
+end
+
+function MOVEMENTMS:KillSelf(Player: Player?) -- Garbage collector
+	self.Player = self.Player or Player
+
+	MOVEMENTMS.Cache[self.Player] = nil
+	setmetatable(self, nil)
+	table.clear(self :: any)
 end
 
 return MOVEMENTMS
